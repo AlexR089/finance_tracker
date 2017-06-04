12a9c095c37cfca53aa966da484d18b31f5aa14d
Alex Ruttkowski <alex.ruttkowski@gmail.com>
2017-06-04 11:47:03 +0000

    Add user and stock association and display

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,8 @@
 class Stock < ActiveRecord::Base
+  
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+  
   # Find the first entry in the local DB which matches the Ticker symbol like 'GOOG'
   def self.find_by_ticker(ticker_symbol)
     where(ticker: ticker_symbol).first
